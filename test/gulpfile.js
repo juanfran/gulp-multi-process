3c9a6b1407cbea8646837c3fc49d5a8bff531e1c
Juanfran <juanfran.ag@gmail.com>
2015-11-24 20:57:23 +0100

    testing

---
+++ test/gulpfile.js
@@ -0,0 +1,24 @@
+var gulpMultiProcess = require('../index.js');
+var gulp = require('gulp');
+
+gulp.task('task1', function(cb) {
+  setTimeout(function() {
+    cb();
+  }, 50);
+});
+
+gulp.task('task2', function(cb) {
+  setTimeout(function() {
+    cb();
+  }, 50);
+});
+
+gulp.task('task3', function(cb) {
+  setTimeout(function() {
+    cb();
+  }, 50);
+});
+
+gulp.task('multi', function(cb) {
+  return gulpMultiProcess(['task1', 'task2', 'task3'], cb);
+});
---
+++ test/main.js
@@ -0,0 +1,17 @@
+var exec = require('child_process').exec;
+var path = require('path');
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('gulp-multi-process', function() {
+  it('run 3 tasks in parallel', function(done) {
+    exec('gulp multi --gulpfile ' + path.join(__dirname, 'gulpfile.js'), function (err, out) {
+      expect(out).to.be.match(/Finished 'task1'/);
+      expect(out).to.be.match(/Finished 'task2'/);
+      expect(out).to.be.match(/Finished 'task3'/);
+      expect(out).to.be.match(/Finished 'multi'/);
+
+      done();
+    });
+  });
+});
