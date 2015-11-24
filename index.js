d314ebeadf8c14ef37c3a4479f1e8f1a2a4599b1
Juanfran <juanfran.ag@gmail.com>
2015-11-24 20:56:53 +0100

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var spawn = require('child_process').spawn;
+
+var gulpMultiProcess = function(tasks, cb) {
+  var completed = 0;
+
+  tasks.forEach(function(taskName) {
+    var worker = spawn('gulp', [taskName] , { stdio: 'inherit' });
+
+    worker.on('exit', function (code) {
+      completed++;
+
+      if(completed === tasks.length) {
+        cb();
+      }
+    });
+  });
+};
+
+module.exports = gulpMultiProcess;
