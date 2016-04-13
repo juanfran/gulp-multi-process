d23bd5378668fef4cd375015642724b380c7b1f3
Juanfran <juanfran.ag@gmail.com>
2016-04-13 18:05:40 +0200

    cb worker code

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ var spawn = require('child_process').spawn;
 
 var gulpMultiProcess = function(tasks, cb) {
   var completed = 0;
+  var code = 0;
 
   tasks.forEach(function(taskName) {
     var worker = spawn(process.argv[1], [taskName] , { stdio: 'inherit' });
 
-    worker.on('exit', function (code) {
+    worker.on('exit', function (workerCode) {
+      if(workerCode !== 0)  {
+        code = workerCode;
+      }
+
       completed++;
 
       if(completed === tasks.length) {
-        cb();
+        cb(code);
       }
     });
   });
