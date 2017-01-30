ab4d3e2a925d5ace51137557816165f9afc64a9a
juanfran <juanfran.ag@gmail.com>
2017-01-30 19:58:03 +0100

    additional arguments child process

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ var gulpMultiProcess = function(tasks, cb) {
   var code = 0;
 
   tasks.forEach(function(taskName) {
-    var worker = spawn(process.execPath, [process.argv[1], taskName] , { stdio: 'inherit' });
+    var args = [process.argv[1], taskName];
+
+    process.argv.forEach(function (val) {
+      if(val[0] === '-' && val !== '--gulpfile') {
+        args.push(val);
+      }
+    });
+
+    var worker = spawn(process.execPath, args , { stdio: 'inherit' });
 
     worker.on('exit', function (workerCode) {
       if(workerCode !== 0)  {
